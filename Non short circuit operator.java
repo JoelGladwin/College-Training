0457b67789350fbd632ab05507bce634b57a1f62
JoelGladwin <160302222+JoelGladwin@users.noreply.github.com>
2025-02-12 11:52:25 +0530

    Add files via upload

---
+++ Non short circuit operator.java
@@ -0,0 +1,16 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a = 2, b = 1,c = 3;
+	    if(a > b & a > c){
+	        System.out.println(a+" is greater");
+	    }
+	    else if(b > c){
+	        System.out.println(b+" is greater");
+	    }
+	    else{
+	        System.out.println(c+" is greater");
+	    }
+	}
+}
