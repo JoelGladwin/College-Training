db617974418104987f13796d1c9163f60284df69
JoelGladwin <160302222+JoelGladwin@users.noreply.github.com>
2025-02-12 12:39:49 +0530

    Add files via upload

---
+++ Bitwise Operator.java
@@ -0,0 +1,11 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a = 492, b = 333;
+	    System.out.println(a&b);
+	    System.out.println(a|b);
+	    System.out.println(a^b);
+	}
+}
+
